4187f7f5876776aef4badf7cef95ed111b0bf9f3
niklaskrauth <niklas.krauth@icloud.com>
2025-03-18 11:01:11 +0100

    Added: password validation

---
+++ src/main/java/org/bilanzius/utils/PasswordValidator.java
@@ -0,0 +1,15 @@
+package org.bilanzius.utils;
+
+import java.util.regex.Pattern;
+
+public class PasswordValidator {
+
+    private static final String PASSWORD_PATTERN =
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,}$";
+    // Needs to contain: 8 chars, 1 digit, 1 lowercase, 1 uppercase, 1 special char
+
+    public static boolean validatePassword(String password) {
+        return Pattern.matches(PASSWORD_PATTERN, password);
+    }
+
+}
